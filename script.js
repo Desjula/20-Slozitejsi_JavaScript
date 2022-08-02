60ba9f068ea77c92b7c4b1538372a9073aab9bc1
Julie Desová <juliedesova@seznam.cz>
2022-08-02 10:50:14 +0200

    hotovo

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("JavaScript funguje!");
+
+let visualniHlavniOdstavec = document.querySelector(".hlavni-odstavec");
+visualniHlavniOdstavec.textContent = "Domovská stránka";
+
+let visualniMojeFoto = document.querySelector(".moje-foto");
+visualniMojeFoto.src = "img/druhe-foto.jpeg"
+
+let visualniPredstaveniSe = document.querySelector(".predstaveni-se");
+visualniPredstaveniSe.style.color = "dodgerblue";
+
+let visualniSkoly = document.querySelector(".vzdelani");
+visualniSkoly.textContent = "Školy";
+
+
+function pridejZvyrazneni(){
+    let visualniTridaTlacitka = document.querySelector(".tlacitko1");
+    visualniTridaTlacitka.classList.add("zvyrazneny");
+}
